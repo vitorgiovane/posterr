231c6803c6d6fe8be068e131731b461b71431973
Vitor Giovane <vitorgpalves@gmail.com>
2022-03-30 20:27:49 -0300

    feature(Global): Install and configure Rack Cors Gem === POS-3

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,12 @@ module Postrr
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
   end
 end
