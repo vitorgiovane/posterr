8ffe238d9a42f96e54e9e46f76ba506956ad8bb9
Vitor Giovane <vitorgpalves@gmail.com>
2022-03-31 16:58:40 -0300

    feature(HomePage): Create seeds to users === POS-21

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+semper = User.create(username: 'semper')
+donec = User.create(username: 'donec')
+vitae = User.create(username: 'vitae')
