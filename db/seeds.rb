1c14fabec01a7ed2cd8b5b6c390c1e4b3ae8615f
Vitor Giovane <vitorgpalves@gmail.com>
2022-04-01 13:04:38 -0300

    feature(HomePage, UserProfilePage):Create seeds to posts === POS-22

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,18 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-semper = User.create(username: 'semper')
 donec = User.create(username: 'donec')
+semper = User.create(username: 'semper')
 vitae = User.create(username: 'vitae')
+
+post_one = Post.create(user: donec, content: 'Fusce quis ante tempus, lobortis magna in, pretium turpis.')
+post_two = Post.create(user: semper, content: 'Suspendisse in mi sit amet nibh efficitur suscipit nec eget felis.')
+post_three = Post.create(user: vitae, content: 'Cras nec tincidunt velit. Pellentesque interdum lacus urna.')
+
+post_four = Post.create(user: semper, parent: post_one)
+post_five = Post.create(user: semper, parent: post_three, content: 'Mauris ac tempus tellus.')
+
+post_six = Post.create(user: vitae, parent: post_five, content: 'Maecenas sit amet blandit nunc, id fermentum turpis.')
+Post.create(user: vitae, parent: post_five)
+
+Post.create(user: donec, parent: post_six)
