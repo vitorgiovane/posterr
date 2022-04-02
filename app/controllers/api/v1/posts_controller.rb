fae42928c8ec2473d13237a1ece3e13930a20907
Vitor Giovane <vitorgpalves@gmail.com>
2022-04-02 19:09:01 -0300

    feature(HomePage): Adds a filter to post listing to allow user posts filtering by users followed by current user
    Now, the request accept a flag param to filter only posts by those you follow or all posts.
    
    ===
    POS-27

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::PostsController < Api::V1::ApiController
   def index
     posts = Post.paginate(page: page, per_page: per_page).order('created_at DESC')
+    posts = posts.where(user_id: current_user.following_ids) if boolean_cast(params[:only_of_the_followed])
 
     render json: posts
   end
--- test/controllers/api/v1/posts_controller_test.rb
+++ test/controllers/api/v1/posts_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class Api::V1::PostsControllerTest < ActionDispatch::IntegrationTest
   test 'should get a list of posts' do
-    get '/api/v1/posts'
+    get base_path
     
     expected_first_post_content = 'Mauris id elit est. Fusce eget ornare leo, sit amet molestie nulla.'
 
@@ -12,7 +12,7 @@ class Api::V1::PostsControllerTest < ActionDispatch::IntegrationTest
 
   test 'should create a post' do
     post_content = 'Lorem ipsum...'
-    post '/api/v1/posts', params: { content: post_content }
+    post base_path, params: { content: post_content }
 
     assert_response :success
     assert_equal post_content, response.parsed_body['content']
@@ -20,9 +20,33 @@ class Api::V1::PostsControllerTest < ActionDispatch::IntegrationTest
 
   test 'should not create a post when parent_id is invalid' do
     nonexistent_post_id = 999
-    post '/api/v1/posts', params: { content: 'Lorem ipsum...', parent_id: nonexistent_post_id }
+    post base_path, params: { content: 'Lorem ipsum...', parent_id: nonexistent_post_id }
 
     bad_request_status_code = 400
     assert_response bad_request_status_code
+  end
+
+  test 'should returns only de posts from who is followed by de current user' do
+    get "#{base_path}?only_of_the_followed=true"
+
+    expected_number_of_posts = 6
+
+    assert_response :success
+    assert_same expected_number_of_posts, response.parsed_body.size, 'Failed to filter posts'
+  end
+
+  test 'should returns all posts' do
+    get "#{base_path}?only_of_the_followed=false"
+
+    expected_number_of_posts = 10
+
+    assert_response :success
+    assert_same expected_number_of_posts, response.parsed_body.size, 'Failed to filter posts'
+  end
+
+  private
+
+  def base_path
+    '/api/v1/posts'
   end
 end
